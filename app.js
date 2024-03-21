61915562e55d83c861d49e9c448f260a2c081cde
Maru <163323276+Maru0803@users.noreply.github.com>
2024-03-20 22:00:33 -0300

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const verificarOrigem = (req, res, next) => {
 const conteudo = { 
     "loja": {  
          "image": "https://i.imgur.com/EZIoltq.png", 
-         "text": "A Baby Dreams Store é a mais recente adição ao mercado ADBL brasileiro, com o objetivo de expandir continuamente e oferecer uma gama cada vez maior de produtos à nossa comunidade, mantendo os preços acessíveis, mesmo para quantidades menores." 
+         "text": "A Baby Dreams Store é a mais recente loja ADBL do brasil, com o objetivo de expandir continuamente e oferecer uma gama cada vez maior de produtos à nossa comunidade, mantendo os preços acessíveis, mesmo para quantidades menores." 
     }, 
     "frete": { 
         "image": "https://i.imgur.com/QJYf6z1.png",
